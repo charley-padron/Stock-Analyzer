ed253707864fe9d26969538d1916cced2319d818
charley-padron <33139667+charley-padron@users.noreply.github.com>
2018-07-01 00:27:25 -0400

    Add files via upload
    Fixed window size and imported libraries needed to continue project

--- StockScreener.py
+++ StockScreener.py
@@ -1,11 +1,29 @@
+#Used for GUI
 from tkinter import *
+
+#Used for plotting the stock data
+import matplotlib as plt
+
+#Used to access stock data
+import alpha_vantage
+
+#alpha_vantage key
+#is private to indivduals 
+#and required to access alpha_vantage
+
+#Used to get the stock data in JSON format 
+import requests
+
+#Used to store stock data
+#And perform calculations for MACD and RSI
+import numpy as np 
 
 class Application(Frame):
     
     def __init__(self, master):
         #Frame.__init__(self, master)
         Frame.__init__(self, master, background="grey")
-        Frame.pack(self)
+        Frame.pack(self, fill="both", expand=True)
         self.create_widgets()
         #self.on_button()
         #self.on_button_exit()
@@ -13,7 +31,7 @@ class Application(Frame):
     def create_widgets(self):
         
         self.label = Label(self, text = "Enter Stocks to search")
-        self.label2 = Label(self, text = "Seperate stocks using a comma!")
+        self.label2 = Label(self, text = "Seperate stocks using a comma and no spaces.")
         self.label.pack()
         self.label2.pack()
         self.entry = Entry(self)
@@ -22,23 +40,36 @@ class Application(Frame):
         print(self.entry.get())
         
         self.button1 = Button(self, text = "Search Stocks", command = self.on_button)
-        self.button1.pack()
+        self.button1.place(x=340,y=65)
         
         self.button2 = Button(self, text = "Exit", command = self.on_button_exit)
-        self.button2.pack()
+        self.button2.place(x=440, y=65)
         
     def on_button(self):
         processInput = self.entry.get()
         print(processInput)
         self.entry.delete(0, 'end')
+        
+        #process the string and continue with project
+        self.stock(processInput)
     
     def on_button_exit(self):
         root.destroy()    
         print("Application ended!")
+    
+    def stock(self, stocks):
+        print(stocks + " new")
+        stocks = stocks.split(',')
+        for stock in stocks:
+           print(stock)
+           
+        #matplotlib in  tkinter
+        #try each symbol, if symbol not found 
+        #return error and continue through list
 
 root = Tk()
 root.title("Stock Screener")
-#root.Frame("800x500")
+root.geometry("800x500")
 
 app = Application(root)
 
